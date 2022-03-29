408056330a61d993db18fb04353d2f0df50bcae5
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-29 22:04:13 +0300

    Flasks class

--- WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
+++ WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+namespace WaterSortPuzzleSolver
+{
+
+    class Flasks
+    {
+       int maxColors = 4;
+
+        List<List<int>> list;
+
+        List<(int, int)> path;
+
+        public Flasks(int maxColors)
+        {
+            this.maxColors = maxColors;
+            list = new List<List<int>>();
+            path = new List<(int, int)>();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
