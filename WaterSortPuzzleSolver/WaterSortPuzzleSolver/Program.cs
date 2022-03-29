5c7e4e7e27f146d4f81ece8d8db7135d3a3010a1
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-29 23:40:59 +0300

    Fixation and tests
    Похоже, хранить в хеш-таблице лист листов не получится, ну или я не понимаю, как она работает.

--- WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
+++ WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
@@ -3,86 +3,88 @@ using System.Collections;
 
 namespace WaterSortPuzzleSolver
 {
-    public class Hashtable
-    {
-        System.Collections.Hashtable hashtable;
+    //public class Hashtable
+    //{
+    //    public System.Collections.Hashtable hashtable;
 
-        public bool Check(ref Flasks newFlasks)
-        {
-            if (hashtable.ContainsKey(newFlasks.flasks))
-            {
-                if ((hashtable[newFlasks.flasks] as Flasks).flasks.Count > newFlasks.flasks.Count)
-                {
-                    hashtable[newFlasks.flasks] = newFlasks;
-                    return false;
-                }
+    //    public bool Check(Flasks newFlasks)
+    //    {
+    //        if (hashtable.ContainsKey(newFlasks.flasksState))
+    //        {
+    //            if ((hashtable[newFlasks.flasksState] as Flasks).path.Count > newFlasks.path.Count)
+    //            {
+    //                hashtable[newFlasks.flasksState] = new Flasks(newFlasks);
+    //                return false;
+    //            }
+                
+    //            return true;
+    //        }
+    //        else
+    //        {
+    //            hashtable.Add(newFlasks.flasksState, newFlasks);
+    //        }
+            
+    //        return false;
+    //    }
 
-                return true;
-            }
-            else
-            {
-                hashtable.Add(newFlasks.flasks, newFlasks);
-            }
+    //    //public Flasks Replacement(Flasks newFlasks)
+    //    //{
+    //    //    return hashtable[newFlasks.flasks] as Flasks;
+    //    //}
 
-            return false;
-        }
+    //    public Hashtable()
+    //    {
+    //        hashtable = new System.Collections.Hashtable();
+    //    }
 
-        //public Flasks Replacement(Flasks newFlasks)
-        //{
-        //    return hashtable[newFlasks.flasks] as Flasks;
-        //}
-
-        public Hashtable()
-        {
-            hashtable = new System.Collections.Hashtable();
-        }
-
-    }
+    //}
     public class Flasks
     {
         int maxColors;
 
-        public List<List<int>> flasks;
+        public List<List<int>> flasksState;
 
         public List<(int, int)> path;
 
-        public void Transfer(int from, int to)
+        public bool Transfer(int from, int to)
         {
-            if (flasks[from].Count > 0 && flasks[to].Count < maxColors)
+            if (flasksState[from].Count > 0 && flasksState[to].Count < maxColors)
             {
-                int t = flasks[from].Last();
-                flasks[to].Add(t);
-                flasks[from].Remove(t);
+                int t = flasksState[from].Last();
+                flasksState[to].Add(t);
+                flasksState[from].Remove(t);
                 path.Add((from, to));
+                return true;
             }
-            else Console.WriteLine("invalid transfer");
+            return false;
         }
+            
 
         public void InitializationRandom()
         {
             var random = new Random();
 
-            int flasksCount = random.Next(2, 10);
+            int flasksCount = random.Next(4, 6);
             for (int i = 0; i < flasksCount; i++)
             {
-                
-                flasks.Add(new List<int>());
+
+                flasksState.Add(new List<int>());
 
                 int flaskCount = random.Next(2, maxColors + 1);
                 for (int j = 0; j < flaskCount; j++)
                 {
-                    flasks[i].Add(random.Next(1,8));
+                    flasksState[i].Add(random.Next(1,8));
                 }
             }
         }
 
         public void Print()
         {
-            for (int i = 0; i < flasks.Count; i++)
+            for (int i = 0; i < flasksState.Count; i++)
             {
-                for (int j = 0; j < flasks[i].Count; j++)
+                for (int j = 0; j < flasksState[i].Count; j++)
                 {
-                    Console.Write(flasks[i][j] + " ");
+                    Console.Write(flasksState[i][j] + " ");
                 }
                 Console.WriteLine();
             }
@@ -91,8 +93,28 @@ namespace WaterSortPuzzleSolver
         public Flasks(int maxColors)
         {
             this.maxColors = maxColors;
-            flasks = new List<List<int>>();
+            flasksState = new List<List<int>>();
             path = new List<(int, int)>();
+        }
+
+        public Flasks(Flasks flasks)
+        {
+            this.maxColors = flasks.maxColors;
+            flasksState = new List<List<int>>();
+            for (int i = 0; i < flasks.flasksState.Count; i++)
+            {
+                flasksState.Add(new List<int>());
+                for (int j = 0; j < flasks.flasksState[i].Count; j++)
+                {
+                    flasksState[i][j] = flasks.flasksState[i][j];
+                }
+                
+            }
+            path = new List<(int, int)>();
+            for (int i = 0; i < flasks.path.Count; i++)
+            {
+                path.Add(flasks.path[i]);
+            }
         }
     }
 
@@ -100,11 +122,27 @@ namespace WaterSortPuzzleSolver
     {
         static void Main(string[] args)
         {
+            Hashtable hashtable = new Hashtable();
             Flasks flasks = new Flasks(4);
             flasks.InitializationRandom();
+            //hashtable.Check(ref flasks);
+             
             flasks.Print();
 
-            Console.WriteLine("Hello World!");
+            var rand = new Random();
+            for (int i = 0; i < 1000;)
+            {
+                if (flasks.Transfer(rand.Next(0, flasks.flasksState.Count), rand.Next(0, flasks.flasksState.Count)))
+                {
+                    i++;
+                    //hashtable.Check(flasks);
+                }
+                    
+                
+            }
+
+            Console.WriteLine();
+            flasks.Print();
         }
     }
 }
