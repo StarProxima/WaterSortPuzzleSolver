88c0927ea8daf2da84339eedcd336535ecd6318e
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-03-31 00:04:40 +0300

    FlasksComparer

--- WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
+++ WaterSortPuzzleSolver/WaterSortPuzzleSolver/Program.cs
@@ -3,7 +3,37 @@ using System.Collections;
 
 namespace WaterSortPuzzleSolver
 {
-    
+
+    public class FlasksComparer : IEqualityComparer<Flasks>
+    {
+        public bool Equals(Flasks x, Flasks y)
+        {
+            if (x.Count != y.Count) return false;
+            for (int i = 0; i < x.Count; i++)
+            {
+                if (x[i].Count != y[i].Count) return false;
+
+                for (int j = 0; j < x[i].Count; j++)
+                {
+                    if (!x[i].SequenceEqual(y[i])) return false;
+                }
+            }
+            return true;
+        }
+
+        public int GetHashCode(Flasks obj)
+        {
+            int hashcode = 0;
+            for (int i = 0; i < obj.Count; i++)
+                for (int j = 0; j < obj[i].Count; j++)
+                {
+                    hashcode += obj[i][j].GetHashCode() ^ j.GetHashCode();
+                }
+            return hashcode;
+
+        }
+    }
+
     public class Flasks : List<List<int>>
     {
         public Flasks(Flasks t) : base()
@@ -26,7 +56,7 @@ namespace WaterSortPuzzleSolver
     {
         
 
-        public Dictionary<Flasks, FlasksStand> hashtable = new Dictionary<Flasks, FlasksStand>();
+        public Dictionary<Flasks, FlasksStand> hashtable = new Dictionary<Flasks, FlasksStand>(new FlasksComparer());
         
         public bool Check(FlasksStand newFlasks)
         {
@@ -50,7 +80,7 @@ namespace WaterSortPuzzleSolver
 
         public HashtableFlask()
         {
-            hashtable = new Dictionary<Flasks, FlasksStand>();
+            hashtable = new Dictionary<Flasks, FlasksStand>(new FlasksComparer());
         }
     }
     public class FlasksStand
